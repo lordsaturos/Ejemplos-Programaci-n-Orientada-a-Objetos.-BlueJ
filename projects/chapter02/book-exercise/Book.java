5af2fa11e0f6a4c363550759ef37bf7c921a741b
lordsaturos <raul.rivero.engineer@gmail.com>
2018-03-30 12:50:18 +0200

    ejercicios repaso tema 2
    Ejercicios de repaso basicos de tema 2 resueltos

--- projects/chapter02/book-exercise/Book.java
+++ projects/chapter02/book-exercise/Book.java
@@ -11,15 +11,23 @@ class Book
     // The fields.
     private String author;
     private String title;
+    private int pages;
+    private String refNumber;
+    private int prestado;
+    private boolean courseText;
 
     /**
      * Set the author and title fields when this object
      * is constructed.
      */
-    public Book(String bookAuthor, String bookTitle)
+    public Book(String bookAuthor, String bookTitle, int pagesCount, boolean bookCourseText)
     {
         author = bookAuthor;
         title = bookTitle;
+        pages = pagesCount;
+        refNumber = "";
+        prestado = 0;
+        courseText = bookCourseText;
     }
 
     // Add the methods here ...
@@ -32,7 +40,27 @@ class Book
     {
         return title;
     }
-    
+
+    public int getPages()
+    {
+        return pages;
+    } 
+
+    public String getRefNumber()
+    {
+        return refNumber;
+    } 
+
+    public int getPrestado()
+    {
+        return prestado;
+    } 
+
+    public boolean isCourseText()
+    {
+        return courseText;
+    }
+
     public void printAuthor()
     {
         System.out.println("El autor es: " + author);
@@ -45,4 +73,37 @@ class Book
         return;
     }
     
+    public void printDetails()
+    {
+        System.out.println("El titulo es: " + title + ". El autor es: " + author + ". El numero de paginas es: " + pages);
+        if (refNumber.length() == 0)
+        {
+            System.out.println("La referencia del libro es: ZZZ");
+        }
+        else
+        {
+            System.out.println("La referencia del libro es: " + refNumber);
+        }
+        System.out.println("El libro ha sido prestado: " + prestado + " veces.");
+    }
+
+    public void setRefNumber(String ref)
+    {
+        if(ref.length() < 3)
+        {
+            System.out.println("La referencia introducida tiene menos de 3 caracteres de largo. Introduzca una referencia correcta");
+        }
+        else
+        {
+            refNumber = ref;
+            System.out.println("La referencia introducida es correcta. La referencia hasido actualizada");
+        }
+        return;
+    }
+
+    public void prestar()
+    {
+        prestado = prestado + 1;
+        return;
+    }
 }
